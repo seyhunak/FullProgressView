2cd07938b70f9094fac6f3b1ea30363d959fd9b5
Seyhun Akyurek <seyhunak@gmail.com>
2015-02-09 11:59:03 +0200

    first commit

---
+++ FullProgressView/FullProgressView.h
@@ -0,0 +1,19 @@
+//
+//  FullProgressView.h
+//  FullProgressView
+//
+//  Created by Seyhun Akyurek on 01/02/15.
+//  Copyright (c) 2015 seyhunak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FullProgressView.
+FOUNDATION_EXPORT double FullProgressViewVersionNumber;
+
+//! Project version string for FullProgressView.
+FOUNDATION_EXPORT const unsigned char FullProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FullProgressView/PublicHeader.h>
+
+
---
+++ FullProgressView/FullProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  FullProgressView.swift
+//  FullProgress
+//
+//  Created by Seyhun Akyurek on 01/02/15.
+//  Copyright (c) 2015 seyhunak. All rights reserved.
+//
+
+import UIKit
+
+class FullProgressView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
