702274343c3a66583b8c79c20a5f69b583a1b84c
Seyhun Akyurek <seyhunak@gmail.com>
2015-02-09 12:03:14 +0200

    FullProgressView class for implementing IBInspectable

--- FullProgressView/FullProgressView.swift
+++ FullProgressView/FullProgressView.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
-class FullProgressView: UIView {
+@IBDesignable class FullProgressView : UIView {
+    
+  var containerView = UIView()
+  var progressView = UIView()
+  var activityIndicator = UIActivityIndicatorView()
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+  class var shared: FullProgressView {
+    
+    struct Static {
+      static let instance: FullProgressView = FullProgressView()
     }
-    */
+    
+    return Static.instance
+  }
+    
+  @IBInspectable var overlayColor: UIColor = UIColor.clearColor() {
+        
+    didSet {
+      layer.backgroundColor = overlayColor.CGColor
+    }
+        
+  }
+
+  @IBInspectable var cornerRadius: CGFloat = 0 {
+    
+    didSet {
+      layer.cornerRadius = cornerRadius
+    }
+    
+  }
 
 }
